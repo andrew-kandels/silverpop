e97f888288d6b44d2015cd690c4ad66317510f4b
Andrew Kandels <akandels@gmail.com>
2013-07-12 16:04:54 -0500

    first commit

---
+++ Module.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Silverpop Project
+ *
+ * This source file is subject to the BSD license bundled with
+ * this package in the LICENSE.txt file. It is also available
+ * on the world-wide-web at http://www.opensource.org/licenses/bsd-license.php.
+ * If you are unable to receive a copy of the license or have
+ * questions concerning the terms, please send an email to
+ * me@andrewkandels.com.
+ *
+ * @category    akandels
+ * @package     furnace
+ * @author      Andrew Kandels (me@andrewkandels.com)
+ * @copyright   Copyright (c) 2013 Andrew P. Kandels (http://andrewkandels.com)
+ * @license     http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @link        http://contain-project.org/furnace
+ */
+
+namespace Silverpop;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\MvcEvent;
+
+/**
+ * ZF2 Module Bootstrap
+ *
+ * @category    akandels
+ * @package     furnace
+ * @copyright   Copyright (c) 2013 Andrew P. Kandels (http://andrewkandels.com)
+ * @license     http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+class Module implements AutoloaderProviderInterface
+{
+    /**
+     * Get Configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * Gets the configuration for autloading Furnace classes.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
